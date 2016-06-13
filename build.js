9fd24535509776477723c74518f61fec415bb935
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-06-13 14:33:20 +0800

    replaced jsmin with uglify

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var jsmin = require('jsmin').jsmin;
+var uglify = require("uglify-js").minify;
 var cssmin = require('cssmin');
 
 var css = fs.readFileSync("src/slide.css", encoding='utf8');
@@ -8,7 +8,7 @@ var js = fs.readFileSync("src/slide.js", encoding='utf8');
 var html = fs.readFileSync("src/slide.html", encoding='utf8');
 
 var inline = html
-  .replace(/<script src="slide.js"><\/script>/, '<script>'+jsmin(js)+'</script>')
+  .replace(/<script src="slide.js"><\/script>/, '<script>'+uglify(js, {fromString: true}).code+'</script>')
   .replace(/<link .*href="slide.css">/, '<style>'+cssmin(css)+'</style>');
 
 console.log(inline);
